91d7d4a9450a18c5918477e69ec4b7a533ff3dd3
Steven Zhao - 332027 <szhao@lanl.gov>
2018-06-20 12:58:23 -0600

    First timer program

---
+++ timers/timertest1.c
@@ -0,0 +1,35 @@
+
+/* A simple timing program, giving the time elapsed in ms to run a program a given amount of times defined by the variable REP_TIMES.
+*/
+
+#include <time.h>
+#include <stdio.h>
+
+
+//The amount of times the program will be performed.
+#define REP_TIMES 1000000000
+
+/* A simple program to test the timer. Returns the some of all elements of a small array A.
+*/
+int A[] = {1,2,4,5};
+int addElements(int arry[]){
+	int sum;
+	int size = sizeof(arry) / sizeof(arry)[0];
+	int j = 0;
+	for (j; j < size; j++)
+		sum+=arry[j];
+	return sum; 
+}	
+
+//The actual timer, elapsed time given in ms.
+int main(){
+    clock_t start = clock();
+	int i =0;
+	int j=0;
+	for (i; i<REP_TIMES; i++)
+		addElements(A);
+		//Currently has the testimg program. Add executable code here!
+    clock_t stop = clock();
+    double elapsed = (double)(stop - start) * 1000 / CLOCKS_PER_SEC;
+    printf("Time elapsed in ms: %f\n", elapsed);
+}
