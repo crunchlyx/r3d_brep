42305271388c6408c19a20ddf839848bfb1ae222
Steven Zhao - 332027 <szhao@lanl.gov>
2018-07-06 13:17:37 -0600

    Successful clip function, working on in-place

--- square2/square2.c
+++ square2/square2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 //The amount of times the program will be performed for timer. 
 #define REP_TIMES 1000000
@@ -10,56 +11,69 @@
 //Largest random coordinate possibly generated
 #define RAND_COORD_LIMIT 10
 
+//The max
+#define MAX_VERTS 64
+
 #define dot(va, vb) (va.x*vb.x + va.y*vb.y)
 
-void clip_poly(r2d_rvec2* verts, r2d_int nverts, r2d_plane* planes, r2d_int nplanes, r2d_rvec2** newverts, r2d_int* finalnverts) {
-	
-	*newverts= (r2d_rvec2 *)malloc(2*nverts*sizeof(r2d_rvec2));
+void inplace_clip_poly(r2d_rvec2* verts, r2d_int *nverts, r2d_plane* planes, r2d_int nplanes) {
 
 	//The likely bound is 3/2nverts; not certain.
-	int lastavail = 0;
-	r2d_real sdists[nverts];
-	const double ZERO = 0.0;
+	r2d_real sdists[*nverts], smin, smax, onv;
+	r2d_int clipped[MAX_VERTS];
+	
+	const r2d_real ZERO = 0.0;
+	
+	
+	for(int p = 0; p < nplanes; ++p) {
+		onv = *nverts;
+		smin = 1.0e30;
+		smax = -1.0e30;
+		memset(&clipped, 0, sizeof(clipped));
 
-	for(int v = 0; v < nverts; ++v) {
-		sdists[v] = planes[0].d + dot(verts[v], planes[0].n);
-		//printf("%f\n", sdists[v]);
-	}
-
-	for(int v = 0; v < nverts; ++v) {
-		if (sdists[v] >= 0) {
-			(*newverts)[lastavail] = verts[v];
-			lastavail++;
+		for(int v = 0; v < onv; ++v) {
+			sdists[v] = planes[p].d + dot(verts[v], planes[p].n);
+			if(sdists[v] < smin) { smin = sdists[v]; } 
+			if(sdists[v] > smax) { smax = sdists[v]; }
+			printf("%f\n", sdists[v]);
+			if(sdists[v] < ZERO) { clipped[v] = 1; }
 		}
-		int vnext = (v+1)%nverts;
-		if (sdists[v] * sdists[vnext] < ZERO) {
-			r2d_real if1 = sdists[vnext] / (sdists[vnext] - sdists[v]);
-			r2d_rvec2 newvert;
-			newvert.x = if1*verts[v].x + (1-if1)*verts[vnext].x;
-			newvert.y = if1*verts[v].y + (1-if1)*verts[vnext].y;
-			(*newverts)[lastavail] = newvert;
-			lastavail++;
-		}	
+
+		if(smin >= 0.0) continue;
+		if(smax <= 0.0) {
+			*nverts = 0;
+			return;
+		}
+		/*for(int v = 0; v < nverts; ++v) {
+			if (sdists[v] >= 0) {
+				newverts[lastavail] = verts[v];
+				lastavail++;
+			}
+			int vnext = (v+1)%nverts;
+			if (sdists[v] * sdists[vnext] < ZERO) {
+				r2d_real if1 = sdists[vnext] / (sdists[vnext] - sdists[v]);
+				r2d_rvec2 newvert;
+				newvert.x = if1*verts[v].x + (1-if1)*verts[vnext].x;
+				newvert.y = if1*verts[v].y + (1-if1)*verts[vnext].y;
+				newverts[lastavail] = newvert;
+				lastavail++;
+			}	
+		}
+		*finalnverts=lastavail;
+*/
+
 	}
-	*finalnverts=lastavail;
-	(*newverts) = (r2d_rvec2 *)realloc((*newverts), lastavail*sizeof(r2d_rvec2));
 }
 
-//frees the memory
-/*void free_poly(r2d_rvec2 *verts, r2d_int nverts) {
-	for(int v=0; v<nverts; v++){
-		r2d_rvec2 *freeptr = verts+v;
-		free(freeptr);
-		printf(" pass");
-	}
-	printf(" pass");
-}*/
+
 
 r2d_rvec2* init_random_poly(int *numverts) {
 
 	int randint = (rand()%8)+3;
 	*numverts = randint;
-	r2d_rvec2* verts = (r2d_rvec2 *)malloc(randint * sizeof(r2d_rvec2));
+	//r2d_rvec2* verts = (r2d_rvec2 *)malloc(randint * sizeof(r2d_rvec2));
+	//static allocation
+	r2d_rvec2* verts = (r2d_rvec2 *)malloc(MAX_VERTS * sizeof(r2d_rvec2));
 
 	for(int i = 0; i < randint; ++i){
 		r2d_real randx = RAND_COORD_LIMIT*(double)rand()/(double)RAND_MAX;
@@ -86,7 +100,21 @@ void info_poly(r2d_rvec2 *poly, r2d_int nverts) {
 	printf("______________________________________________\n");
 }
 
-int main(){
+//provisional runner
+int main() {
+	r2d_int nverts;
+	srand(time(0));
+
+	r2d_plane planes[] = {{{1,0}, -5.0}};
+	int nplanes = sizeof(planes) / sizeof(planes[0]); // number of clipping planes
+
+	r2d_rvec2 *randpoly = init_random_poly(&nverts);
+	inplace_clip_poly(randpoly, &nverts, planes, nplanes);
+	printf("%d\n", nverts);
+}
+
+
+/*int main(){
 
 	//number of vertices
 	r2d_int nverts;
@@ -105,19 +133,20 @@ int main(){
 
 		// print the poly
 		//info_poly(randpoly, nverts);
+		r2d_poly polygon;
+		r2d_init_poly(&polygon, randpoly, nverts); 
+		r2d_clip(&polygon, planes, nplanes);
+
 
 		// clip the poly using our prototype function
-		r2d_rvec2* finalverts;
-		r2d_int finalnverts;
-		clip_poly(randpoly, nverts, planes, nplanes, &finalverts, &finalnverts);
+		inplace_clip_poly(randpoly, nverts, planes, nplanes);
 
 		// print the clipped poly
 		//info_poly(finalverts, finalnverts);
 
 		//r2d
-		r2d_poly polygon;
-		r2d_init_poly(&polygon, randpoly, nverts); 
-		r2d_clip(&polygon, planes, nplanes);
+
+
 		
 		//free the poly
 		//free_poly(finalverts, finalnverts);
@@ -125,4 +154,4 @@ int main(){
 	clock_t stop3 = clock();
 	double elapsed3 = (double)(stop3 - start3) * 1000 / CLOCKS_PER_SEC;
 	printf("Time elapsed in ms, us: %f\n", elapsed3); 
-}
+}*/
