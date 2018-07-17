8eba5bb9dde2971a59ed3edf9d1ad5e45a437961
Steven Zhao - 332027 <szhao@lanl.gov>
2018-07-17 16:28:10 -0600

    Created r3d driver

--- cube/cube1.c
+++ cube/cube1.c
@@ -2,77 +2,69 @@
 #include "r3d.h"
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-//The amount of times the program will be performed for timer.
-#define REP_TIMES 1
+#define dot(va, vb) (va.x * vb.x + va.y * vb.y + va.z * vb.z)
 
-// A simple printer that prints all BREP information provided to it
-void printer(int faces, int* verts, int** faceindices)
-{
-	printf("Number of faces: %d\n", faces);
-	printf("# of vertices by face: \n");	
-	for (int k = 0; k<faces; ++k)
-		printf("%d ", *(verts+k));
-	printf("\nVertex indices: \n");	
-	for (int i = 0; i<faces; ++i)
-	{
-		for (int j = 0; j<*(verts+i); ++j)
-		{
-			printf("%d ", *(*(faceindices+i)+j));
-		}
-		printf("\n");
+void r3d_clip_brep(r3d_brep* poly, r3d_plane* planes, r3d_int nplanes){
+	
+	const double ZERO = 0.0;
+	r3d_int nverts = poly->numvertices;
+	r3d_rvec3* verts = poly->vertices;
+	r3d_int nfaces = poly->numfaces;
+	r3d_int* nvertsperface = poly->numvertsperface;
+	r3d_int** faceinds = poly->faceinds;
+
+	r3d_real sdists[R3D_MAX_VERTS];
+
+	for(int v = 0; v < nverts; ++v) {
+		sdists[v] = planes[0].d + dot(verts[v], planes[0].n);
 	}
+	
+	r3d_int vnext;
+	for(int f = 0; f < nfaces; ++f) {
+		for(int v = 0; v < nvertsperface[f]; ++v) {
+			vnext = (v+1)%nvertsperface[f];
+			if (sdists[faceinds[f][v]] * sdists[faceinds[f][vnext]] < ZERO){
+				printf("Cross @ face %d\n", f);
+			}
+		}
+	}		
+
+
 }
 
-//This is for a cube!
-int main(){
+int main() {
 
-	int nfaces = 6; // number of faces in poly
-	int nvertsperface[] = {4,4,4,4,4,4}; // number of vertices per face
+	r3d_int nverts = 8;
+	r3d_int nfaces = 6;
+	r3d_int nvertsperface[6] = {4,4,4,4,4,4};
+	r3d_int f0[4] = {0,1,5,4};
+	r3d_int f1[4] = {1,2,6,5};
+	r3d_int f2[4] = {2,3,7,6};
+	r3d_int f3[4] = {0,4,7,3};
+	r3d_int f4[4] = {0,3,2,1};
+	r3d_int f5[4] = {4,5,6,7};
+	r3d_int* faceinds[6] = {&f0, &f1, &f2, &f3, &f4, &f5};
+	r3d_rvec3 verts[R3D_MAX_VERTS] = {{0,0,0}, {1,0,0}, {1,1,0}, {0,1,0},
+					 				 {0,0,1}, {1,0,1}, {1,1,1}, {0,1,1}}; 
+
+	r3d_plane planes[] = {{{1,0,0}, -0.5}};
+	r3d_int nplanes = sizeof(planes) / sizeof(planes[0]);
+
+	r3d_poly cube;
+	//r3d_init_poly(&cube, verts, nverts, faceinds, nvertsperface, nfaces);
+	//r3d_clip(&cube, planes, nplanes); 
+	//r3d_print(&cube);
+
+	r3d_brep poly;
+	poly.numvertices = nverts;
+	poly.vertices = verts;
+	poly.faceinds = faceinds;
+	poly.numvertsperface = nvertsperface;
+	poly.numfaces = nfaces;
+
+	r3d_clip_brep(&poly, planes, nplanes);
 
 
-	r3d_rvec3 verts[] = {
-		{0,0,0},{1,0,0},{1,1,0},{0,1,0},
-		{0,0,1},{1,0,1},{1,1,1},{0,1,1}}; //the vertices
-	int nverts = sizeof(verts)/sizeof(verts)[0]; // the number of vertices
-	
-	// the faces, holds the brep of each face
-	int f0[] = {2,6,5,1};
-	int f1[] = {1,5,4,0};
-	int f2[] = {4,7,3,0};
-	int f3[] = {7,6,2,3};
-	int f4[] = {6,7,4,5};
-	int f5[] = {3,2,1,0};
-	int* faceinds[] = {f0,f1,f2,f3,f4,f5}; // the list of all faces as defined by the brep
-
-	r3d_poly cube; // the cube
-	r3d_plane planes[] = {{{1,0,0}, -0.5}}; // the clipping planes
-	int nplanes = sizeof(planes) / sizeof(planes[0]); // number of clipping planes
-
-	// variables for clipping and converting to brep
-	r3d_brep *cuberep;
-	r3d_int numcomponents;
-	
-	//timer
-    clock_t start = clock();
-	for (int i = 0; i<REP_TIMES; i++){
-		r3d_init_poly(&cube, verts, nverts, faceinds, nvertsperface, nfaces); 
-		r3d_clip(&cube, planes, nplanes);
-		r3d_init_brep(&cube, &cuberep, &numcomponents);
-	}
-
-	clock_t stop = clock();
-    double elapsed = (double)(stop - start) * 1000 / CLOCKS_PER_SEC;
-    printf("Time elapsed in ms: %f\n", elapsed);
-	
-	/*
-	r3d_init_poly(&cube, verts, nverts, faceinds, nvertsperface, nfaces);
-	r3d_print(&cube);
-	printf("\n");
-	r3d_clip(&cube, planes, nplanes);
-	r3d_print(&cube);
-	printf("\n");
-	r3d_print_brep(&cuberep, numcomponents);
-	*/
-	
 }
--- square3/square3.c
+++ square3/square3.c
@@ -4,162 +4,19 @@
 #include <stdlib.h>
 
 //The amount of times the program will be performed for timer. 
-//#define REP_TIMES 10000000
+#define REP_TIMES 10000000
 //#define REP_TIMES 2
-#define REP_TIMES 1
+//#define REP_TIMES 1
 
 //Largest random coordinate possibly generated
 #define RAND_COORD_LIMIT 10
 
-// Buffer limit
-#define MAX_VERTS 64
-#define dot(va, vb) (va.x*vb.x + va.y*vb.y)
-
-//free allocated memory
-/*void free_poly(r2d_brep* poly) {
-	free(poly->verts);
-}*/
-
-void r2d_split_brep(r2d_brep* poly, r2d_int npolys, r2d_plane plane, r2d_brep* outpoly_pos, r2d_brep* outpoly_neg){
-
-	// variable declarations
-	const r2d_real ZERO = 0.0;	
-	r2d_int vnext, nverts, outpos_nverts, outneg_nverts;	
-	r2d_rvec2 *verts, *outpos_verts, *outneg_verts;
-
-	for(int p = 0; p < npolys; ++p) {
-
-		nverts = poly[p].nverts;
-		verts = poly[p].verts; 
-		outpos_nverts=0;
-		outneg_nverts=0;
-		// direct access to the vertex buffer
-		verts = poly[p].verts;
-		outpos_verts = outpoly_pos[p].verts;
-		outneg_verts = outpoly_neg[p].verts;
-		nverts = poly[p].nverts;
-
-		r2d_real sdist_cur = plane.d + dot(verts[0], plane.n);
-
-		// loop over each vertex
-		for(int v = 0; v < nverts; ++v) {
-
-			// if on correct side, add vertex coordinates to the next avaiable slot
-			// in the buffer
-			if (sdist_cur >= 0) {
-				outpos_verts[outpos_nverts++] = verts[v];
-			}
-			if (sdist_cur < 0) {
-				 outneg_verts[outneg_nverts++] = verts[v];
-			}
-		
-			vnext = (v+1)%nverts;
-			r2d_real sdist_next= plane.d + dot(verts[vnext], plane.n);
-	
-			// interpolate new vertex if edge crosses and add vertex coordinates to
-			// next available slot int the buffer
-			if (sdist_cur * sdist_next < ZERO) {
-				r2d_real alpha = sdist_next / (sdist_next - sdist_cur);
-				outneg_verts[outneg_nverts].x = outpos_verts[outpos_nverts].x = alpha*verts[v].x + (1-alpha)*verts[vnext].x;
-				outneg_verts[outneg_nverts].y = outpos_verts[outpos_nverts].y = alpha*verts[v].y + (1-alpha)*verts[vnext].y;
-				outpos_nverts++;
-				outneg_nverts++;
-			}	
-			sdist_cur=sdist_next;
-		}
-		// update value of nverts of the polys
-		outpoly_pos[p].nverts = outpos_nverts;
-		outpoly_neg[p].nverts = outneg_nverts;
-	}
-}
-
-void r2d_reduce_brep(r2d_brep* poly, r2d_real* moments, r2d_int polyorder) {
-
-	// var declarations
-	r2d_int vcur, vnext, m, i, j, corder;
-	r2d_real twoa;
-	r2d_rvec2 v0, v1; 
-
-	// direct access to vertex buffer
-	r2d_rvec2* vertbuffer = poly->verts; 
-	r2d_int* nverts = &poly->nverts; 
-
-	// zero the moments
-	for(m = 0; m < R2D_NUM_MOMENTS(polyorder); ++m)
-		moments[m] = 0.0;
-
-	if(*nverts <= 0) return;
-
-	// Storage for coefficients
-	// keep two layers of the triangle of coefficients
-	r2d_int prevlayer = 0;
-	r2d_int curlayer = 1;
-	r2d_real D[polyorder+1][2];
-	r2d_real C[polyorder+1][2];
-
-	// iterate over edges and compute a sum over simplices 
-	for(vcur = 0; vcur < *nverts; ++vcur) {
-
-		vnext = (vcur+1)%(*nverts);
-		v0.x = vertbuffer[vcur].x;
-		v0.y = vertbuffer[vcur].y;
-		v1.x = vertbuffer[vnext].x;
-		v1.y = vertbuffer[vnext].y;
-		twoa = (v0.x*v1.y - v0.y*v1.x); 
-
-		// calculate the moments
-		// using the fast recursive method of Koehl (2012)
-		// essentially building a set of Pascal's triangles, one layer at a time
-
-		// base case
-		D[0][prevlayer] = 1.0;
-		C[0][prevlayer] = 1.0;
-		moments[0] += 0.5*twoa;
-
-		// build up successive polynomial orders
-		for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-			for(i = corder; i >= 0; --i, ++m) {
-				j = corder - i;
-				C[i][curlayer] = 0; 
-				D[i][curlayer] = 0;  
-				if(i > 0) {
-					C[i][curlayer] += v1.x*C[i-1][prevlayer];
-					D[i][curlayer] += v0.x*D[i-1][prevlayer]; 
-				}
-				if(j > 0) {
-					C[i][curlayer] += v1.y*C[i][prevlayer];
-					D[i][curlayer] += v0.y*D[i][prevlayer]; 
-				}
-				D[i][curlayer] += C[i][curlayer]; 
-				moments[m] += twoa*D[i][curlayer];
-			}
-			curlayer = 1 - curlayer;
-			prevlayer = 1 - prevlayer;
-		}
-	}
-
-	// reuse C to recursively compute the leading multinomial coefficients
-	C[0][prevlayer] = 1.0;
-	for(corder = 1, m = 1; corder <= polyorder; ++corder) {
-		for(i = corder; i >= 0; --i, ++m) {
-			j = corder - i;
-			C[i][curlayer] = 0.0; 
-			if(i > 0) C[i][curlayer] += C[i-1][prevlayer];
-			if(j > 0) C[i][curlayer] += C[i][prevlayer];
-			moments[m] /= C[i][curlayer]*(corder+1)*(corder+2);
-		}
-		curlayer = 1 - curlayer;
-		prevlayer = 1 - prevlayer;
-	}
-}
-
-//generate a random poly in a buffer of the right size, for use by clip_poly and r2d_clip
 void init_random_poly(r2d_rvec2* verts, r2d_int* nverts) {
 
-	//generate random number of vertices
-	int randint = (rand()%8)+3;
+	// generate random number of vertices
+	//int randint = (rand()%8)+3;
 
-	//int randint = 4;
+	int randint = 15;
 	*nverts = randint;
 
 	for(int i = 0; i < randint; ++i){
@@ -184,36 +41,20 @@ void init_nice_square(r2d_rvec2* verts, r2d_int* nverts) {
 	verts[3].y = 1.0;
 }
 
-void info_poly2(r2d_brep* poly) { 
-	printf("\n______________________________________________\n");
-	
-	r2d_rvec2* verts = poly->verts;
-	r2d_int nverts = poly->nverts;
-	//number of vertices	
-	printf("%d vertices in poly: \n", nverts);
-	
-	//all vertex coordinates
-	printf("Vertex coordinates:\n");
-	for (int i = 0; i < nverts; ++i) {
-		r2d_rvec2 printvert = verts[i];
-
-		printf("%f ", printvert.x);
-		printf("%f\n", printvert.y);
-	}
-	printf("______________________________________________\n");
-}
-
-
 int main(){
 
 	r2d_int nverts;
-	r2d_int npoly;
+	r2d_int npoly, npoly2;
 	srand(time(0));
 	r2d_rvec2 randverts[R2D_MAX_VERTS];
+	r2d_real momentspos[1];
+	r2d_real momentspos2[1];
+	r2d_real momentsneg[1];
+	r2d_real momentspneg2[1];
 	
 	// the intersection planes
-	//r2d_plane planes[] = {{{1,0}, -5.0}, {{0,1}, -5.0}, {{-1,-1}, -15.0}};
-	//int nplanes = sizeof(planes) / sizeof(planes[0]); // number of clipping planes
+	r2d_plane planes[] = {{{1,0}, -2.0}, {{1,0}, -5.0},{{1,0}, -8.0}};
+	int nplanes = sizeof(planes) / sizeof(planes[0]); // number of clipping planes
 
 	r2d_plane plane = {{1,0}, -5.0};
 	// simple timer loop	
@@ -222,47 +63,64 @@ int main(){
 		
 		init_random_poly(randverts, &nverts);
 
-		r2d_brep outpolypos[2];
-		r2d_brep outpolyneg[2];
-		r2d_brep inpolys[2];
-		for (int k = 0; k < 2; ++k){
+		r2d_brep outpolypos[1];
+		r2d_brep outpolyneg[1];
+		r2d_brep inpolys[1];
+		for (int k = 0; k < 1; ++k){
 			for (int j =0; j<nverts; ++j) {
 				inpolys[k].verts[j]=randverts[j];
 			}
 			inpolys[k].nverts = nverts;
 		}
-		npoly = 2;
+		npoly = 1;
 
 
 		r2d_split_brep(&inpolys, npoly, plane, &outpolypos, &outpolyneg);
-		for (int l = 0; l < npoly; ++l) {
-			r2d_print_brep(&outpolypos[l]);
-			r2d_print_brep(&outpolyneg[l]);
-		}
-		/*
-		r2d_brep polybrep;
+		r2d_reduce_brep(&outpolypos, &momentspos2, 1);
+		r2d_reduce_brep(&outpolyneg, &momentsneg2, 1);
+		/*r2d_brep polybrep;
 		for (int j =0; j<nverts; ++j) {
 			polybrep.verts[j]=randverts[j];
 		}
 		polybrep.nverts = nverts;
-
 		r2d_brep polybrep2;
 		for (int j =0; j<nverts; ++j) {
 			polybrep2.verts[j]=randverts[j];
 		}
-		polybrep2.nverts = nverts;
+		polybrep2.nverts = nverts;*/
 
 		// R2D:
-		r2d_poly polygon;
+		r2d_poly polygon[1];
+		r2d_poly r2d_outpos[1];
+		r2d_poly r2d_outneg[1];
 		r2d_init_poly(&polygon, randverts, nverts); 
+		npoly2 = 1;
+		
+		r2d_split(&polygon, npoly2, plane, &r2d_outpos, &r2d_outneg);
+		r2d_reduce(&r2d_outpos, &momentspos, 1);
+		r2d_reduce(&r2d_outneg, &momentsneg, 1);
+
+		for(int z = 0; z < 1; ++z){
+			if(momentspos[z] != momentspos2[z]){
+				printf("component = %d\n", z);
+				printf("Moment1 = %f\n", momentspos[z]);
+				printf("Moment2 = %f\n", momentspos2[z]);
+			}
+		}
+		for(int z = 0; z < 1; ++z){
+			if(momentsneg[z] != momentsneg2[z]){
+				printf("component = %d\n", z);
+				printf("Moment1 = %f\n", momentsneg[z]);
+				printf("Moment2 = %f\n", momentsneg2[z]);
+			}
+		}
+
+		/*r2d_clip(&polygon, planes, nplanes);
 		//r2d_print(&polygon);
-		r2d_clip(&polygon, planes, nplanes);
-		r2d_print(&polygon);
 
 		// Our work buffer
-		r2d_print_brep(&polybrep2);
 		r2d_clip_brep(&polybrep2, planes, nplanes);
-		r2d_print_brep(&polybrep2);*/
+		//r2d_print_brep(&polybrep2);*/
 	}
 	clock_t stop3 = clock();
 	double elapsed3 = (double)(stop3 - start3) * 1000 / CLOCKS_PER_SEC;
